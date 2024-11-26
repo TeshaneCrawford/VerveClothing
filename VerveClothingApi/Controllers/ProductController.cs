ef656f8a0b1f4f3b482f3f81844abf10b4a1d3e1
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-25 21:39:43 -0500

    Add ProductController and update project structure
    A new ProductController class has been added to handle API requests related to products.

---
+++ VerveClothingApi/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VerveClothingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
