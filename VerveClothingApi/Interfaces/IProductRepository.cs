f366859fa8c575e7dd429d6bd40b2c62311fde56
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-30 12:24:49 -0500

    Add new methods for filtering, searching, and stock quantity retrieval in IProductRepository

--- VerveClothingApi/Interfaces/IProductRepository.cs
+++ VerveClothingApi/Interfaces/IProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using VerveClothingApi.DTOs;
+using VerveClothingApi.Common;
 
 namespace VerveClothingApi.Interfaces
 {
@@ -9,5 +10,8 @@ namespace VerveClothingApi.Interfaces
         Task<ProductDto> CreateAsync(CreateProductDto createProductDto);
         Task<ProductDto> UpdateAsync(int id, UpdateProductDto updateProductDto);
         Task<bool> DeleteAsync(int id);
+        Task<PagedResult<ProductDto>> GetFilteredAsync(ProductFilterParams filterParams);
+        Task<IEnumerable<ProductDto>> SearchByTermAsync(string searchTerm);
+        Task<int> GetStockQuantityAsync(int productId);
     }
 }
